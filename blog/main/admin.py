26b392c99419055bcc76c0bbb58abe776ddd2ff6
Lasloflo <88950535+Lasloflo@users.noreply.github.com>
2021-11-25 18:58:46 +0300

    admin commit

--- blog/main/admin.py
+++ blog/main/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('author',)
+    search_fields = ('title',)
 
-    admin.site.register(Post)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'create_at', 'is_active')
+    list_editable = ('is_active',)
+    search_fields = ('author', 'post')
+    list_filter = ('author', 'post')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
